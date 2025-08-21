fd6c3e01a7243ca98c067d44a856bdf604b3fd0b
David Reband <david.reband@webanizer.de>
2025-08-21 17:59:50 +0200

    rename Namecoin to Doichain

--- src/common/args.cpp
+++ src/common/args.cpp
@@ -36,7 +36,7 @@
 #include <utility>
 #include <variant>
 
-const char * const BITCOIN_CONF_FILENAME = "namecoin.conf";
+const char * const BITCOIN_CONF_FILENAME = "doichain.conf";
 const char * const BITCOIN_SETTINGS_FILENAME = "settings.json";
 
 ArgsManager gArgs;
@@ -723,18 +723,18 @@ bool HasTestOption(const ArgsManager& args, const std::string& test_option)
 fs::path GetDefaultDataDir()
 {
     // Windows:
-    //   old: C:\Users\Username\AppData\Roaming\Namecoin
-    //   new: C:\Users\Username\AppData\Local\Namecoin
-    // macOS: ~/Library/Application Support/Namecoin
-    // Unix-like: ~/.namecoin
+    //   old: C:\Users\Username\AppData\Roaming\Doichain
+    //   new: C:\Users\Username\AppData\Local\Doichain
+    // macOS: ~/Library/Application Support/Doichain
+    // Unix-like: ~/.doichain
 #ifdef WIN32
     // Windows
     // Check for existence of datadir in old location and keep it there
-    fs::path legacy_path = GetSpecialFolderPath(CSIDL_APPDATA) / "Namecoin";
+    fs::path legacy_path = GetSpecialFolderPath(CSIDL_APPDATA) / "Doichain";
     if (fs::exists(legacy_path)) return legacy_path;
 
     // Otherwise, fresh installs can start in the new, "proper" location
-    return GetSpecialFolderPath(CSIDL_LOCAL_APPDATA) / "Namecoin";
+    return GetSpecialFolderPath(CSIDL_LOCAL_APPDATA) / "Doichain";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -744,10 +744,10 @@ fs::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef __APPLE__
     // macOS
-    return pathRet / "Library/Application Support/Namecoin";
+    return pathRet / "Library/Application Support/Doichain";
 #else
     // Unix-like
-    return pathRet / ".namecoin";
+    return pathRet / ".doichain";
 #endif
 #endif
 }
